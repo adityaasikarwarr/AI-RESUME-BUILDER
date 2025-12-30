81cfbd16f2ce332a9d7f6243d13b1355babe28f4
adityasikarwarr <adityasikarwarr156@gmail.com>
2025-12-30 21:27:37 +0530

    made changes in index css file

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -55,13 +55,13 @@ const Dashboard = () => {
           >
             <PlusIcon className="size-11 transition-all duration-300 p-2.5 bg-linear-to-br from-indigo-300 to-indigo-500 text-white rounded-full" />
             <p className="text-sm group-hover;text-indigo-600 transition-all duration-300">
-              create Resume
+              Create Resume
             </p>
           </button>
           <button className="w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-purple-500 hover:shadow-lg transition-all duration-300 cursor-pointer">
-            <UploadCloudIcon className="size-11 transition-all duration-300 p-2.5 bg-linear-to-br from-purple-300 to-purple-500 text-white rounded-full" />
+            <UploadCloudIcon className="size-11 transition-all duration-300 p-2.5 bg-linear-to-br from-purple-300 to-purple-500 text-white rounded-ful   @apply l" />
             <p className="text-sm group-hover;text-indigo-600 transition-all duration-300">
-              Upload existing Resume
+              Upload Existing Resume
             </p>
           </button>
         </div>
