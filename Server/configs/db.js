b0e035bb758c7cf6c82909705ceb28e19762979b
adityasikarwarr <adityasikarwarr156@gmail.com>
2026-01-04 20:25:43 +0530

    connecting mongodb to the server

---
+++ Server/configs/db.js
@@ -0,0 +1,25 @@
+import mongoose, { mongo } from "mongoose";
+
+const connectDB = async () => {
+  try {
+    mongoose.connect.on("connected", () => {
+      console.log("MongoDB connected successfully");
+    });
+
+    let mongodbURI = process.env.MONGO_URI;
+    const projectName = "resume-builder";
+
+    if (!mongodbURI) {
+      throw new Error("MONGO_URI is not defined in environment variables");
+    }
+
+    if (mongodbURI.endsWith("/")) {
+      mongodbURI = mongodbURI.slice(0, -1);
+    }
+
+    await mongoose.connect(`${mongodbURI}/${projectName}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+export default connectDB;
